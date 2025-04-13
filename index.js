f3441fe30104775c75f7f4749f7547e8066e44c4
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-04-12 21:30:02 -0300

    Adiconei diferenciação de chat e sistema para enviar e trocar mensagens.

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+var usuarios = [];
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-
-
 io.on('connection',(socket)=>{
     socket.on('chat message', (obj)=>{
-        console.log(obj);
+        io.emit('chat message', obj);
     });
 
 });
-
-
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
